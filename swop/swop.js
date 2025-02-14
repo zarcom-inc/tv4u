c867129d9d3487accada84e2710aca5901a91450
zarcom-inc <zarcom.inc@gmail.com>
2025-02-13 16:42:25 -0800

    exact copy swop
    fully copied the swop

--- swop/swop.js
+++ swop/swop.js
@@ -1,52 +1,189 @@
-// swop.js
-// This script works with your existing stbplayer and prov.js files.
-// It replaces the original drm-play swop page functionality.
+// Global variables for messages
+var sErrorCode = 'Incorrect Сode!',
+    sErrorFileSize = 'File size limited!!!',
+    sFileUploaded = 'File uploaded!',
+    sValueSended = 'Value sended!';
 
-// When the user types into the code field, enable the "Check" button when the code length equals 6.
-function doChangeCode(){
-  var code = $('#inputCode').val().trim();
-  $('#btnCheck').prop('disabled', code.length !== 6);
+function dochangecode() {
+  $('#btnCheck').prop('disabled', $('#inputCode').val().length != 6);
 }
 
-// When the user clicks "Check", verify the code.
-function doCheck(){
-  var code = $('#inputCode').val().trim();
-  // Here, "123456" is used as the valid code.
-  // Replace with your own code‐validation logic as needed.
-  var validCode = "123456";
-  
-  if (code === validCode) {
-    // If the code is correct, disable further editing and show the playlist (m3u) form.
-    $('#inputCode').prop('disabled', true);
-    $('#btnCheck').hide();
-    $('#divSend-m3u').show();
-    $('#inputName-m3u').focus();
-  } else {
-    alert("Incorrect Code!");
-  }
+function doCheck() {
+  $.ajax({
+    url: '/swop/a.php',
+    data: { c: 'check', d: $('#inputCode').val() },
+    type: 'post',
+    success: function (json) {
+      if (json.data === 'send') {
+        $('#inputCode').prop('disabled', true);
+        $('#btnCheck').hide();
+        $('#btnDownload').show().focus();
+      } else if (json.data === 'get') {
+        $('#inputCode').prop('disabled', true);
+        $('#btnCheck').hide();
+        $('#btnUpload').show().focus();
+      } else if (json.data === 'var') {
+        $('#inputCode').prop('disabled', true);
+        $('#btnCheck').hide();
+        $('#divSend').show();
+        $('#lVar').html(json.name + ':');
+        $('#inputVar').val(json.val).focus();
+      } else if (json.data === 'm3u') {
+        $('#inputCode').prop('disabled', true);
+        $('#btnCheck').hide();
+        $('#divSend-m3u').show();
+        $('#name-m3u').html(json.name + ':');
+        $('#inputName-m3u').val(json.val).focus();
+        $('#www-m3u').html(json.www + ':');
+        $('#inputWww-m3u').val(json.val).focus();
+        $('#medUrl-m3u').html(json.medUrl + ':');
+        $('#inputMedUrl-m3u').val(json.val).focus();
+      } else if (json.data === 'ed') {
+        $('#inputCode').prop('disabled', true);
+        $('#btnCheck').hide();
+        $('#divSend-ed').show();
+        $('#edkey').html(json.edkey + ':');
+        $('#input-edkey').val(json.val).focus();
+        $('#vpurl').html(json.vpurl + ':');
+        $('#input-vpurl').val(json.val).focus();
+        $('#edurl').html(json.edurl + ':');
+        $('#input-edurl').val(json.val).focus();
+      } else {
+        alert(sErrorCode);
+      }
+    },
+    error: function (jqXHR) {
+      alert(sErrorCode);
+      console.log('error:' + JSON.stringify(jqXHR));
+    }
+  });
 }
 
-// When the user submits the m3u playlist form.
-function doSend_m3u(){
-  var playlistName = $('#inputName-m3u').val().trim();
-  var playlistURL  = $('#inputWww-m3u').val().trim();
-  var mediaURL     = $('#inputMedUrl-m3u').val().trim();
+function doDownload() {
+  window.open('/swop/a.php?c=download&d=' + $('#inputCode').val(), '_blank');
+  location.reload();
+}
 
-  if (!playlistURL) {
-    alert("Please enter a valid m3u URL.");
+function doFindFile() {
+  $('#my_hidden_file').val('');
+  $('#my_hidden_file').click();
+}
+
+function doUpload() {
+  if ($('#my_hidden_file').prop('files')[0].size > 300000) {
+    alert(sErrorFileSize);
     return;
   }
-  
-  // Optionally, save the playlist info using your prov.js functions.
-  // For example: provSetPlaylist(playlistName, playlistURL, mediaURL);
-  
-  // Now, integrate with your stbplayer.
-  // For example, call stbPlay() with the m3u URL.
-  stbPlay(playlistURL);
-  
-  // Inform the user that the playlist is loaded.
-  alert("Playlist loaded into player!");
-  
-  // Optionally, redirect or reload your player view:
-  // window.location.href = "player.html?m3u=" + encodeURIComponent(playlistURL);
+  var form_data = new FormData();
+  form_data.append('c', 'upload');
+  form_data.append('d', $('#inputCode').val());
+  form_data.append('file', $('#my_hidden_file').prop('files')[0]);
+  $.ajax({
+    url: '/swop/a.php',
+    dataType: 'text',
+    cache: false,
+    contentType: false,
+    processData: false,
+    data: form_data,
+    type: 'post',
+    success: function (php_script_response) {
+      alert(sFileUploaded);
+      location.reload();
+    },
+    error: function (jqXHR) {
+      alert(jqXHR.responseText);
+      console.log('error:' + JSON.stringify(jqXHR));
+    }
+  });
 }
+
+function doSend() {
+  $.ajax({
+    url: '/swop/a.php',
+    data: { c: 'send_val', d: $('#inputCode').val(), v: $('#inputVar').val() },
+    type: 'post',
+    success: function (json) {
+      alert(sValueSended);
+      window.location.href = window.location.href.split('?')[0];
+    },
+    error: function (jqXHR) {
+      alert(jqXHR.responseText);
+      console.log('error:' + JSON.stringify(jqXHR));
+    }
+  });
+}
+
+function doSend_m3u() {
+  $.ajax({
+    url: '/swop/a.php',
+    data: {
+      c: 'send_m3u',
+      d: $('#inputCode').val(),
+      name: $('#inputName-m3u').val(),
+      www: $('#inputWww-m3u').val(),
+      medUrl: $('#inputMedUrl-m3u').val()
+    },
+    type: 'post',
+    success: function (json) {
+      alert(sValueSended);
+      window.location.href = window.location.href.split('?')[0];
+    },
+    error: function (jqXHR) {
+      alert(jqXHR.responseText);
+      console.log('error:' + JSON.stringify(jqXHR));
+    }
+  });
+}
+
+function doSend_ed() {
+  $.ajax({
+    url: '/swop/a.php',
+    data: {
+      c: 'send_ed',
+      d: $('#inputCode').val(),
+      edkey: $('#input-edkey').val(),
+      vpurl: $('#input-vpurl').val(),
+      edurl: $('#input-edurl').val()
+    },
+    type: 'post',
+    success: function (json) {
+      alert(sValueSended);
+      window.location.href = window.location.href.split('?')[0];
+    },
+    error: function (jqXHR) {
+      alert(jqXHR.responseText);
+      console.log('error:' + JSON.stringify(jqXHR));
+    }
+  });
+}
+
+$(document).ready(function () {
+  var l = (navigator.language || '').substr(0, 2);
+  switch (l) {
+    case 'ru':
+      $('#inputCode').attr('placeholder', 'Код');
+      $('#btnCheck').text('Проверить');
+      $('#btnDownload').text('Скачать');
+      $('#btnUpload').text('Отправить');
+      $('#btnSend').text('Отправить');
+      sErrorCode = 'Неверный код!';
+      sErrorFileSize = 'Размер файла ограничен!!!';
+      sFileUploaded = 'Файл отправлен!';
+      sValueSended = 'Значение отправлено!';
+      break;
+    case 'uk':
+      $('#inputCode').attr('placeholder', 'Код');
+      $('#btnCheck').text('Перевірити');
+      $('#btnDownload').text('Завантажити');
+      $('#btnUpload').text('Надіслати');
+      $('#btnSend').text('Надіслати');
+      sErrorCode = 'Невірний код!';
+      sErrorFileSize = 'Розмір файлу обмежений!!!';
+      sFileUploaded = 'Файл відправлений!';
+      sValueSended = 'Значення відправлено!';
+      break;
+  }
+  $('#inputCode').val((window.location.href.split('?')[1] || '').split('&')[0]);
+  dochangecode();
+  if ($('#inputCode').val().length === 6) doCheck();
+});
